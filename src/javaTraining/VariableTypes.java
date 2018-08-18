970057949a381292a6c2dbb25bb113df62f3d4b1
sanjaya <sanjayahalwala@gmail.com>
2018-08-19 00:14:41 +0530

    Initial commit

---
+++ src/javaTraining/VariableTypes.java
@@ -0,0 +1,16 @@
+package javaTraining;
+
+public class VariableTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 11;
+		int z;
+		z = x+y;
+		
+		System.out.println("sum of x and y:"+ z);
+
+	}
+
+}
